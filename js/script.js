f4770649a4c4083cd7fd52fec7cbdb69e23be838
Simi O <coolkid1984@ymail.com>
2023-10-27 18:41:20 -0500

    added ui logic

--- js/script.js
+++ js/script.js
@@ -14,31 +14,31 @@ function replaceWithBeep(numberList) {
 }
 
 function replaceWithBoop(numberList) {
-  return numberList.map(function(number) {
-    return number.toString().includes('2') ? 'Boop!' : number;
-  });
+  return numberList.map(function (number) {
+    return number.toString().includes("2") ? "Boop!" : number
+  })
 }
 
 function replaceWithDJRobo(numberList) {
-  return numberList.map(function(number) {
-    return number.toString().includes('3') ? 'DJ Robo on the beat!!' : number;
-  });
+  return numberList.map(function (number) {
+    return number.toString().includes("3") ? "DJ Robo on the beat!!" : number
+  })
 }
 
 function handleNonNumericInput(input) {
   if (isNaN(input) || input === "") {
-    return "Error: Please enter a valid number.";
+    return "Error: Please enter a valid number."
   }
-  return parseInt(input);
+  return parseInt(input)
 }
 
 function handleNegativeNumbers(userInput) {
-  var absoluteInput = Math.abs(userInput);
-  return generateRapLyrics(absoluteInput);
+  var absoluteInput = Math.abs(userInput)
+  return generateRapLyrics(absoluteInput)
 }
 
 function handleZeroInput() {
-  return [0];
+  return [0]
 }
 
 function generateRapLyrics(userInput) {
@@ -54,12 +54,26 @@ function generateRapLyrics(userInput) {
   return processedNumbers
 }
 
-//ui logic 
+//ui logic
 
-document.addEventListener("DOMContentLoaded", function() {
-  
-  var numberInput = document.getElementById("number");
-  var rapButton = document.getElementById("rapButton");
-  var resultsList = document.getElementById("results");
+document.addEventListener("DOMContentLoaded", function () {
+  var numberInput = document.getElementById("number")
+  var rapButton = document.getElementById("rapButton")
+  var resultsList = document.getElementById("results")
 
-});
+  function updateResults() {
+    var userInput = numberInput.value
+
+    var rapLyrics = generateRapLyrics(userInput)
+
+    resultsList.innerHTML = ""
+
+    rapLyrics.forEach(function (lyric) {
+      var listItem = document.createElement("li")
+      listItem.textContent = lyric
+      resultsList.appendChild(listItem)
+    })
+  }
+
+  rapButton.addEventListener("click", updateResults)
+})
