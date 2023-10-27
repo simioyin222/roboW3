ebe6ad63235f2a4ae8483622e5a149df191a5fb2
Simi O <coolkid1984@ymail.com>
2023-10-27 17:17:08 -0500

    pass second test

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 // Business Logic
 function generateNumberList(userInput) {
-  var numberList = [];
+  var numberList = []
   for (var i = 0; i <= userInput; i++) {
-    numberList.push(i);
+    numberList.push(i)
   }
-  return numberList;
+  return numberList
+}
+
+function replaceWithBeep(numberList) {
+  return numberList.map(function (number) {
+    return number.toString().includes("1") ? "Beep!" : number
+  })
 }
