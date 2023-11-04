d6c7543ebabf4e4c04ed4eb4874910602f775d43
Simi O <coolkid1984@ymail.com>
2023-11-03 19:04:42 -0500

    add new functions to javascript and remove old ones

--- js/script.js
+++ js/script.js
@@ -27,49 +27,19 @@ function generateRapLyrics(userInput) {
     return [processedInput];
   }
 
-function replaceWithBeep(numberList) {
-  return numberList.map(function (number) {
-    return number.toString().includes("1") ? "Beep!" : number;
-  });
-}
-
-function replaceWithBoop(numberList) {
-  return numberList.map(function (number) {
-    return number.toString().includes("2") ? "Boop!" : number;
-  });
-}
-
-function replaceWithDJRobo(numberList) {
-  return numberList.map(function (number) {
-    return number.toString().includes("3") ? "DJ Robo on the beat!!" : number;
-  });
-}
-
-function handleNonNumericInput(input) {
-  if (isNaN(input) || input === "") {
-    return "Error: Please enter a valid number.";
+  let numberList = [];
+  for (let i = 0; i <= processedInput; i++) {
+    let numberString = i.toString();
+    if (numberString.includes("3")) {
+      numberList.push("DJ Robo on the beat!!");
+    } else if (numberString.includes("2")) {
+      numberList.push("Boop!");
+    } else if (numberString.includes("1")) {
+      numberList.push("Beep!");
+    } else {
+      numberList.push(i);
+    }
   }
-  return parseInt(input);
+  return numberList;
 }
 
-function handleNegativeNumbers(userInput) {
-  var absoluteInput = Math.abs(userInput);
-  return generateRapLyrics(absoluteInput);
-}
-
-function handleZeroInput() {
-  return [0];
-}
-
-function generateRapLyrics(userInput) {
-  var processedNumbers = generateNumberList(handleNonNumericInput(userInput));
-  processedNumbers = replaceWithBeep(processedNumbers);
-  processedNumbers = replaceWithBoop(processedNumbers);
-  processedNumbers = replaceWithDJRobo(processedNumbers);
-  if (userInput < 0) {
-    processedNumbers = handleNegativeNumbers(userInput);
-  } else if (userInput === 0) {
-    processedNumbers = handleZeroInput();
-  }
-  return processedNumbers;
-}
